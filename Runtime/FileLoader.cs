831ee54f0deb2e4d90ed7ef745b576a0526f976c
uurha <uurha@users.noreply.github.com>
2022-11-21 22:58:37 +0000

    Merge pull request #4 from uurha/uurha-patch-1
    Update upm-publish.yml

--- Runtime/FileLoader.cs
+++ Runtime/FileLoader.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using BetterExtensions.Runtime.Extension;
@@ -8,35 +9,6 @@ namespace BuildNotification.Runtime
 {
     public static class FileLoader
     {
-        public static async Task<T> LoadSecuredFile<T>(string path, string salt) where T : class
-        {
-            if (File.Exists(path))
-            {
-#if UNITY_2021_3_OR_NEWER
-                var str = await File.ReadAllTextAsync(path);
-#else
-                var str = File.ReadAllText(path);
-#endif
-                var decrypted = await EncryptService.DecryptStringToBytes(str, salt);
-                return await decrypted.DeserializeAsync<T>();
-            }
-
-            return default;
-        }
-
-
-        public static async void SaveSecuredFile<T>(string path, T data, string salt) where T : class
-        {
-            var serialized = await data.SerializeAsync();
-            var secured = await EncryptService.EncryptString(serialized, salt);
-
-#if UNITY_2021_3_OR_NEWER
-            await File.WriteAllTextAsync(path, secured);
-#else
-            File.WriteAllText(path, secured);
-#endif
-        }
-
         public static async Task<T> LoadFile<T>(string path) where T : class
         {
             if (File.Exists(path))
@@ -46,7 +18,12 @@ namespace BuildNotification.Runtime
 #else
                 var str = File.ReadAllText(path);
 #endif
-                return await Convert.FromBase64String(str).DeserializeAsync<T>();
+                return await Task.Run(() =>
+                {
+                    var fromBase64String = Convert.FromBase64String(str);
+                    var value = Encoding.UTF8.GetString(fromBase64String);
+                    return JsonConvert.DeserializeObject<T>(value);
+                });
             }
 
             return default;
@@ -77,9 +54,14 @@ namespace BuildNotification.Runtime
 
         public static async Task SaveFile<T>(string path, T data) where T : class
         {
-            var serialized = await data.SerializeAsync();
+            var serialized = await Task.Run(() =>
+            {
+                var serializeObject = JsonConvert.SerializeObject(data, Formatting.Indented);
+                var bytes = Encoding.UTF8.GetBytes(serializeObject);
+                return Convert.ToBase64String(bytes);
+            });
 #if UNITY_2021_3_OR_NEWER
-            await File.WriteAllTextAsync(path, Convert.ToBase64String(serialized));
+            await File.WriteAllTextAsync(path, serialized);
 #else
             File.WriteAllText(path, Convert.ToBase64String(serialized));
 #endif
